0c2ba3c8438e362189d8ab12e74e2364e551e28d
IvanCdeveloper <166036220+IvanCdeveloper@users.noreply.github.com>
2025-11-07 23:53:30 +0100

    v2.0.0

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -9,9 +9,9 @@ export const environment = {
 
   //URLS
   // Para desarrollo
-  /*   baseUrl: 'http://localhost:8080/api' */
+    baseUrl: 'http://localhost:8080/api'
   // Para producción
   /* baseUrl: 'https://ivancuarteros.com/api' */
   // Para producción con docker
-  baseUrl: "/api"
+  /* baseUrl: "/api" */
 }
